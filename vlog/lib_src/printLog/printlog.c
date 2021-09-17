e3d434a02379e88d831d0cc03b04530ea92ff289
LJTian <tianlijun315@gmail.com>
2021-09-17 11:27:39 +0800

    整理代码

--- vlog/lib_src/printLog/printlog.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "../../include/vlog.h"
-#include "printlog.h"
-
-int printlog(){
-	vLog(LOG_ERROR,"%s","printLog.so");
-	return 0;
-}
--- vlog/lib_src/printLog/printlog.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PRINTFLOG_H
-#define __PRINTFLOG_H
-
-int printflog();
-
-#endif
