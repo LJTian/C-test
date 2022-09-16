744adec10e7cf73e00a75f37d6e9e1444f401728
ljTian <tianlijun@uniontech.com>
2022-09-16 14:37:42 +0800

    上传内存占用代码

--- Linux/cgroup_mem/main.c
+++ Linux/cgroup_mem/main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define M_BYTE 1024*1024
+#include <unistd.h> 
 
-void main()
+#define M_BYTE (1<<20)
+
+int main()
 {
-	void *Malloc[10];
+	void *Malloc[20];
 	int i;
+	int num;
+	printf( "请输入num:\n");
+	scanf( "%d", &num );
+	printf( "m = %d\n", num);
  
-	for ( i = 0 ; i< 9 ; i++ )
+	for ( i = 0 ; i < num ; i++ )
 	{
-		Malloc[i] = (char *)malloc( 100 *  M_BYTE );
-		memset( Malloc[i], 'A', 100 * M_BYTE );
-		//sleep(1000);
+		Malloc[i] = (char *)malloc( 1024 *  M_BYTE );
+		memset( Malloc[i], 'A', 1024 * M_BYTE );
+		memset( Malloc[i] + (1024 * M_BYTE) - 1 , '\0', 1 );
+		sleep(1);
+		printf("%d\n", i);
 	}
 
 	while(1)
-		sleep(5000);
+		sleep(5);
 
-	for ( i = 0 ; i < 10; i++ )
-		free(Malloc[i]);
-
+	return 0;
 }
